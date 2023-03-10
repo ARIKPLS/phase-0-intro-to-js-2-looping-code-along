18830e4ed536f8f1c78359c9370345573f483eed
EricVentura <ericvent@outlook.com>
2023-03-10 14:01:56 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+ function writeCards( names, event ) {
+  let giftCards = []
+  for ( let i = 0; i < names.length; i++ ) {
+    giftCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+  }
+  return giftCards
+}
+
+function countDown(){
+    let numCountdown=11;
+    while(numCountdown--){
+      console.log(numCountdown)
+    }
+  } 
+  
